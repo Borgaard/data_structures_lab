a17a316490ba280eeddf7d047e5b4fb201147634
Cho S. Kim <choskim@Chos-MacBook-Pro-2.local>
2015-02-02 14:39:51 -0800

    Adds balance_parens.js

---
+++ balance_parens.js
@@ -0,0 +1,20 @@
+/* 
+  Today, we learned about stacks and queues. You'll have to use one of these data structures to answer the following question:
+
+  Create a function that will return true if a string contains balanced parentheses. 
+
+  sample input: balanceParens("(())")
+  sample output: true
+
+  sample input: balanceParens("()()")
+  sample output: true
+
+  sample input: balanceParens("()(")
+  sample output: false
+
+  (What's the relevance? Imagine how an interpreter knows that this is a syntax error: function() {)
+*/
+
+var balanceParens = function(string) {
+  // your solution goes here	
+};
