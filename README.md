971af934e10a471ed31283a0f5430e5edfded5f8
Cho S. Kim <choskim.me@gmail.com>
2015-02-03 14:34:51 -0800

    Adds node_from_tail.js

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 For each of the five days we explore data structures, you will find at least one question testing your understanding of the content of that day. Please look below to know which questions to answer: 
 
 1. Monday: balanceParens 
+2. Tuesday: nodeFromTail
---
+++ node_from_tail.js
@@ -0,0 +1,21 @@
+/* 
+  Today, we learned about singly-linked lists. To challenge your understanding of this data structure, try to solve the following problem:
+
+  Create a function that will return the node located n-positions from the end of a list.
+  
+  sample 
+  list.add("one");
+  list.add("two");
+  list.add("three");
+  list.add("four");  
+
+  sample input: nodeFromTail = function(0)
+  sample output: {data: "four", next: null}
+
+  sample input: nodeFromTail = function(1)
+  sample output: {data: "two", next: null}
+*/
+
+var nodeFromTail = function(index) {
+  // your solution goes here	
+};
